41b8b7a9aa0d4e23a301674eb44b3264a2b579e4
Andrew <akariaka@yandex.ru>
2026-04-17 20:21:38 +0600

    add sass

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -3,5 +3,33 @@
 </script>
 
 <template>
-  
+  <div class="app">
+    <div class="wrapper">
+      <h1 class="title">Калькулятор</h1>
+      <div class="calc">
+        <div class="output"></div>
+        <header class="calc__header">
+          <div class="calc__header__input"></div>
+          <button class="btn delete" id="btn_delete">Удалить</button>
+        </header>
+      </div>
+    </div>
+   
+  </div>
 </template>
+
+<style lang="scss">
+  .wrapper{
+  width: 100%;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.title{
+  margin-bottom: 30px;
+}
+
+</style>
